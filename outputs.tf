613e3cdde83c312eedb119c263670158006ab270
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:42 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "sentinel_alert_rule_threat_intelligences_id" {
+  description = "Map of id values across all sentinel_alert_rule_threat_intelligences, keyed the same as var.sentinel_alert_rule_threat_intelligences"
+  value       = { for k, v in azurerm_sentinel_alert_rule_threat_intelligence.sentinel_alert_rule_threat_intelligences : k => v.id }
+}
 output "sentinel_alert_rule_threat_intelligences_alert_rule_template_guid" {
   description = "Map of alert_rule_template_guid values across all sentinel_alert_rule_threat_intelligences, keyed the same as var.sentinel_alert_rule_threat_intelligences"
   value       = { for k, v in azurerm_sentinel_alert_rule_threat_intelligence.sentinel_alert_rule_threat_intelligences : k => v.alert_rule_template_guid }
